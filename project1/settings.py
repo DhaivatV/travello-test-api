26762f49f3259f6d21195448b7af2ac208b493ed
DhaivatV <71979720+DhaivatV@users.noreply.github.com>
2021-11-26 11:25:05 +0530

    commit

--- project1/settings.py
+++ project1/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@8!=^g@@8&5v5se(w*40f-upmbvl+7hxkx24bf8#8s7iw2k^w@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['djangotravello.herokuapp.com', '127.0.0.1']
 
