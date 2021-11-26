5005729f97c6c537464d56600eb4f22c48c914e7
DhaivatV <71979720+DhaivatV@users.noreply.github.com>
2021-11-26 12:48:16 +0530

    commit

--- project1/settings.py
+++ project1/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:\\Users\\dvipa\\OneDrive\\Desktop\\Django\\project1\\templates'],
+        'DIRS': os.path.join(BASE_DIR, 'templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
